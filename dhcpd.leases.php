9b0618e160d4458df8fecbd3c1a14bb4cab17bf2
Stephan Plepelits <Stephan Plepelits>
2011-03-30 13:06:05 +0200

    Split in several files

---
+++ dhcpd.leases.php
@@ -0,0 +1,47 @@
+<?
+include "conf.php";
+
+$contents=file_get_contents("/var/lib/dhcp3/dhcpd.leases");
+$contents=explode("\n", $contents);
+
+$current=0;
+foreach($contents as $line) {
+  switch($current) {
+    case 0:
+      if(preg_match("/^\s*(|#.*)$/", $line, $m)) {
+      }
+      else if(preg_match("/^lease (.*) {/", $line, $m)) {
+        $current=$m[1];
+      }
+      else {
+        print "Failed parsing '$line'\n";
+      }
+      break;
+    default:
+      if(preg_match("/^\s*([a-z\-]+) (.*);$/", $line, $m)) {
+        $data[$current][$m[1]]=$m[2];
+      }
+      elseif(preg_match("/}/", $line, $m)) {
+        $current=0;
+      }
+      else {
+        print "Failed parsing '$line'\n";
+      }
+  }
+}
+
+$count_active=array();
+foreach($pools as $pool_id=>$p) {
+  $count_active[$pool_id]=0;
+}
+
+foreach($data as $ip=>$d) {
+  if($d['binding']=="state active") {
+    foreach($pools as $pool_id=>$p) {
+      if(preg_match("/$p/", $ip)) {
+        $count_active[$pool_id]++;
+      }
+    }
+  }
+}
+
